237a75b622f507474fc09262386df05ace0eccbb
Yashodash-19 <yashodash19@gmail.com>
2025-02-06 15:13:50 +0530

    185-186

---
+++ Assignment_185.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class Assignment_185 {
+
+	public static void main(String[] args) 
+	{
+		String username=args[0];
+		String password=args[1];
+		System.out.println("Username is:"+username);
+		System.out.println("Password is:"+password);
+
+	}
+
+}
---
+++ Assignment_186.java
@@ -0,0 +1,14 @@
+package assignments;
+
+import java.util.Arrays;
+
+public class Assignment_186 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(Arrays.toString(args));
+		
+
+	}
+
+}
